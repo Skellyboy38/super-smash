28e553e6776084bd2541bd4a595e3eb8a18d77dc
skellyboy38 <mcauley.robert@live.com>
2015-05-09 00:55:04 -0400

    Changed the way we draw to the screen
    Instead of using the spriteBatch object, I switched to using the Stage
    object. The stage object is built into the API and it already uses a
    camera and spriteBatch in its implementation, AND it allows us to add
    buttons and pictures pretty easily. From now on we should pass the stage
    object into the constructors of new classes instead of a spriteBatch.

--- core/src/com/mygdx/game/screens/HomeScreen.java
+++ core/src/com/mygdx/game/screens/HomeScreen.java
@@ -1,8 +1,16 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.mygdx.game.MyGdxGame;
 
 public class HomeScreen extends Screen{
@@ -10,17 +18,39 @@ public class HomeScreen extends Screen{
 	Button settings;
 	Button quit;
 	SpriteBatch batch;
-	Texture background;
+	Texture picture;
+	Image background;
 	float locationX;
 	boolean moveRight;
 	
-	public HomeScreen(SpriteBatch batch) {
-		this.batch = batch;
-		play = new Button();
-		play.setName("Play");
-		settings = new Button();
-		quit = new Button();
-		background = new Texture("background.jpg");
+	Stage stage;
+    TextButton button;
+    TextButtonStyle textButtonStyle;
+    BitmapFont font;
+    Skin skin;
+    TextureAtlas buttonAtlas;
+	
+	public HomeScreen(Stage stage) {
+		this.stage = stage;
+		skin = new Skin();
+		locationX = 0;
+		
+		buttonAtlas = new TextureAtlas("buttons/buttons.pack");
+		picture = new Texture("background.jpg");
+		background = new Image(picture);
+		skin.addRegions(buttonAtlas);
+        Gdx.input.setInputProcessor(stage);
+        font = new BitmapFont();
+        
+        textButtonStyle = new TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = skin.getDrawable("button");
+        button = new TextButton("Push Me", textButtonStyle);
+        button.setPosition(100, 100);
+        button.setHeight(300);
+        button.setWidth(300);
+        stage.addActor(background);
+        stage.addActor(button);
 	}
 	
 	public void update() {
@@ -32,10 +62,13 @@ public class HomeScreen extends Screen{
 			moveRight = false;
 		if(locationX <= 0)
 			moveRight = true;
+		background.setPosition(locationX, 0);
 	}
 	
 	public void render() {
 		update();
-		batch.draw(background, locationX, 0);
+		stage.act();
+		stage.draw();
+		
 	}
 }
