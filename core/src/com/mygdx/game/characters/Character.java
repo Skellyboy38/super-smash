b6a4f285e5c2202f79b1c0aeaeb7b3aec7a8146c
David Dube <daviddube91@hotmail.com>
2015-08-19 19:52:42 -0400

    Added All Character States

--- core/src/com/mygdx/game/characters/Character.java
+++ core/src/com/mygdx/game/characters/Character.java
@@ -8,6 +8,36 @@ public class Character {
 	private WalkingState walking;
 	private RunningState running;
 	private JumpingState jumping;
+	private HangingState hanging;
+	
+	private NeutralAState neutralA;
+	private SideSmashAState sideSmashA;
+	private UpSmashAState upSmashA;
+	private DownSmashAState downSmashA;
+	private SideTiltAState sideTiltA;
+	private UpTiltAState upTiltA;
+	private DownTiltAState downTiltA;
+	private DashAState dashA;
+	private NeutralAirAState neutralAirA;
+	private SideForwardAirAState sideForwardAirA;
+	private SideBackwardAirAState sideBackwardAirA;
+	private UpAirAState upAirA;
+	private DownAirAState downAirA;
+	private ReturnFromHangingAState returnFromHangingA;
+	
+	private NeutralBState neutralB;
+	private SideBState sideB;
+	private UpBState upB;
+	private DownBState downB;
+	
+	private GrabState grab;
+	private ThrowForwardState throwForward;
+	private ThrowBackwardState throwbackward;
+	private ThrowUpState throwUp;
+	private ThrowDownState throwDown;
+	
+	private ShieldState shield;
+	
 	private State currentState;		//The current state points to whatever state the character is in.
 
 	private float posX;		//The current position of the character in screen coordinates
@@ -18,6 +48,8 @@ public class Character {
 		walking = new WalkingState();
 		running = new RunningState();
 		jumping = new JumpingState();
+		hanging = new HangingState();
+		
 		currentState = standing;		//Initially the character is standing.
 	}
 
@@ -83,5 +115,405 @@ public class Character {
 
 		}
 
+	}
+	
+	private class HangingState extends State {
+		Texture image;
+		Animation animation;
+		public HangingState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class NeutralAState extends State {
+		Texture image;
+		Animation animation;
+		public NeutralAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class SideSmashAState extends State {
+		Texture image;
+		Animation animation;
+		public SideSmashAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class UpSmashAState extends State {
+		Texture image;
+		Animation animation;
+		public UpSmashAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class DownSmashAState extends State {
+		Texture image;
+		Animation animation;
+		public DownSmashAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class SideTiltAState extends State {
+		Texture image;
+		Animation animation;
+		public SideTiltAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class UpTiltAState extends State {
+		Texture image;
+		Animation animation;
+		public UpTiltAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class DownTiltAState extends State {
+		Texture image;
+		Animation animation;
+		public DownTiltAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class DashAState extends State {
+		Texture image;
+		Animation animation;
+		public DashAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class NeutralAirAState extends State {
+		Texture image;
+		Animation animation;
+		public NeutralAirAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class SideForwardAirAState extends State {
+		Texture image;
+		Animation animation;
+		public SideForwardAirAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class SideBackwardAirAState extends State {
+		Texture image;
+		Animation animation;
+		public SideBackwardAirAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class UpAirAState extends State {
+		Texture image;
+		Animation animation;
+		public UpAirAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class DownAirAState extends State {
+		Texture image;
+		Animation animation;
+		public DownAirAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class ReturnFromHangingAState extends State {
+		Texture image;
+		Animation animation;
+		public ReturnFromHangingAState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class NeutralBState extends State {
+		Texture image;
+		Animation animation;
+		public NeutralBState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class SideBState extends State {
+		Texture image;
+		Animation animation;
+		public SideBState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class UpBState extends State {
+		Texture image;
+		Animation animation;
+		public UpBState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class DownBState extends State {
+		Texture image;
+		Animation animation;
+		public DownBState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class GrabState extends State {
+		Texture image;
+		Animation animation;
+		public GrabState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class ThrowForwardState extends State {
+		Texture image;
+		Animation animation;
+		public ThrowForwardState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class ThrowBackwardState extends State {
+		Texture image;
+		Animation animation;
+		public ThrowBackwardState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class ThrowUpState extends State {
+		Texture image;
+		Animation animation;
+		public ThrowUpState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class ThrowDownState extends State {
+		Texture image;
+		Animation animation;
+		public ThrowDownState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
+	}
+	
+	private class ShieldState extends State {
+		Texture image;
+		Animation animation;
+		public ShieldState() {
+
+		}
+
+		public void update() {
+
+		}
+
+		public void render() {
+
+		}
 	}
 }
