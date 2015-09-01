dfe4413e6b3ff6d52fb6ee0288d4bd4f17252485
Robert McAuley <mcauley.robert@live.com>
2015-08-31 23:15:19 -0400

    Finished collision boxes for final destination
    While in game, you can press "T" to see the collision boxes (they are
    normally invisible). Press "T" to toggle between visible and invisible
    =)

--- core/src/com/mygdx/game/stages/FinalDestination.java
+++ core/src/com/mygdx/game/stages/FinalDestination.java
@@ -8,15 +8,40 @@ import com.mygdx.game.MyGdxGame;
 public class FinalDestination extends Map{
 
 	public FinalDestination(SpriteBatch batch) {
+		
 		Texture stage = new Texture("stages/final_destination/final_destination.png");
 		Texture background = new Texture("stages/final_destination/final_destination_background.png");
-		Rectangle[] rectangles = new Rectangle[1];
-		Rectangle main = new Rectangle();
-		main.setHeight(MyGdxGame.GAME_HEIGHT/8);
-		main.setWidth((MyGdxGame.GAME_WIDTH - stage.getWidth())/2);
-		rectangles[0] = main;
+		Rectangle[] rectangles = new Rectangle[4];
+		
 		posX = (MyGdxGame.GAME_WIDTH - stage.getWidth())/2;
 		posY = MyGdxGame.GAME_HEIGHT/6;
+		
+		Rectangle main = new Rectangle();		//Creating the bounding rectangles for collision detection
+		Rectangle under = new Rectangle();
+		Rectangle leftEdge = new Rectangle();
+		Rectangle rightEdge = new Rectangle();
+		
+		main.setHeight(stage.getHeight());		//Setting the parameters of the bounding rectangles
+		main.setWidth(stage.getWidth());
+		main.setPosition(posX, posY);
+		
+		under.setHeight(main.getHeight()/10);
+		under.setWidth(main.getWidth());
+		under.setPosition(posX, posY - under.getHeight());
+		
+		leftEdge.setHeight(main.getHeight()*0.8f);
+		leftEdge.setWidth(10);
+		leftEdge.setPosition(posX - leftEdge.getWidth(), posY);
+		
+		rightEdge.setHeight(leftEdge.getHeight());
+		rightEdge.setWidth(leftEdge.getWidth());
+		rightEdge.setPosition(posX + main.getWidth(), posY);
+		
+		rectangles[0] = main;
+		rectangles[1] = under;
+		rectangles[2] = leftEdge;
+		rectangles[3] = rightEdge;
+		
 		create(stage, background, rectangles, batch);
 	}
 	
--- core/src/com/mygdx/game/stages/Map.java
+++ core/src/com/mygdx/game/stages/Map.java
@@ -1,7 +1,10 @@
 package com.mygdx.game.stages;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Map {
@@ -9,12 +12,18 @@ public class Map {
 	private Texture background;
 	private Rectangle[] rectangles;
 	private SpriteBatch batch;
+	private ShapeRenderer shapeRenderer;
 	
 	protected float posX;
 	protected float posY;
 	
+	boolean showBoxes;
+	boolean keyHolding;
+	
 	public Map() {
-		
+		showBoxes = false;
+		keyHolding = false;
+		shapeRenderer = new ShapeRenderer();
 	}
 	
 	public void create(Texture stage, Texture background, Rectangle[] rectangles, SpriteBatch batch) {
@@ -22,6 +31,7 @@ public class Map {
 		this.background = background;
 		this.rectangles = rectangles;
 		this.batch = batch;
+		shapeRenderer.setColor(1,1,0,1);
 	}
 	
 	public float getPosX() {
@@ -41,13 +51,30 @@ public class Map {
 	}
 	
 	public void render() {
+		update();
 		batch.begin();
 		batch.draw(background, 0, 0);
 		batch.draw(stage, posX, posY);
+		if(showBoxes) {			//Draw all the collision boxes
+			shapeRenderer.begin(ShapeType.Line);
+			for(int i = 0; i < rectangles.length; i++) {
+				shapeRenderer.rect(rectangles[i].getX(), rectangles[i].getY(), rectangles[i].getWidth(), rectangles[i].getHeight());
+			}
+			shapeRenderer.end();
+		}
 		batch.end();
 	}
 	
+	public Rectangle[] getRectangles() {
+		return rectangles;
+	}
+	
 	public void update() {
-		
+		if(Gdx.input.isKeyPressed(48) && !keyHolding) {		//If "T" is pressed, testing mode is entered.
+			showBoxes = !showBoxes;
+			keyHolding = true;
+		}
+		else if(!Gdx.input.isKeyPressed(48))
+			keyHolding = false;
 	}
 }
