ec75b9b4d119932af00855616493ea67b846f726
skellyboy38 <mcauley.robert@live.com>
2016-01-07 15:39:45 -0500

    Revert "I added a useless long"
    This reverts commit 8900d9c6f469c75c13cf43e9a42081bc2dc07b6a.

--- core/src/com/mygdx/game/screens/HomeScreen.java
+++ core/src/com/mygdx/game/screens/HomeScreen.java
@@ -32,7 +32,6 @@ public class HomeScreen extends Screen{
 	long r = 0;			//Values used to alter the color of the text for the play button.
 	long g = 0;
 	long b = 0;
-	long toDelete = 0;
 	
 	public HomeScreen(MyGdxGame game, SpriteBatch batch) {
 		super(game);	
@@ -48,7 +47,6 @@ public class HomeScreen extends Screen{
 	public void addScreens(Screen next) {
 		this.next = next;
 	}
-
 	
 	public void create() {
 		picture = new Texture("homeScreen/background.png");
